68e87b89fd3ad783524449489069481c143a22ed
Timofey Lyssenko <tim.lyssenko@gmail.com>
2017-11-06 21:33:41 +0300

    Add simple virtualenv indicator to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,3 +15,12 @@ alias vimbigfile 'vim -n -u NONE -i NONE -N'
 set -x PATH "/home/asleap/.pyenv/bin" $PATH
 status --is-interactive; and . (pyenv init -|psub)
 status --is-interactive; and . (pyenv virtualenv-init -|psub)
+
+# Show virtualenv
+functions -c fish_prompt _old_fish_prompt
+function fish_prompt
+    if set -q VIRTUAL_ENV
+        echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+    end
+    _old_fish_prompt
+end
