eaa1336fe21a0cc52c851384af249e4fb5a47de7
Timofey Lyssenko <tim.lyssenko@gmail.com>
2017-11-26 21:23:37 +0300

    Update fish_prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,4 +1,3 @@
-# Defined in /home/asleap/.config/fish/functions/fish_prompt.fish @ line 1
 function fish_prompt
   # Cache exit status
   set -l last_status $status
@@ -20,23 +19,21 @@ function fish_prompt
   set -l normal (set_color normal)
   set -l cyan (set_color cyan)
   set -l yellow (set_color yellow)
-  set -l bpurple (set_color -o purple)
-  set -l bred (set_color -o red)
-  set -l bcyan (set_color -o cyan)
-  set -l bwhite (set_color -o white)
+  set -l bgreen (set_color -o green)
+  set -l red (set_color red)
 
   # Configure __fish_git_prompt
   set -g __fish_git_prompt_show_informative_status true
   set -g __fish_git_prompt_showcolorhints true
 
   # Color prompt char red for non-zero exit status
-  set -l pcolor $bpurple
+  set -l pcolor $normal
   if [ $last_status -ne 0 ]
-    set pcolor $bred
+    set pcolor $red
   end
 
   # Top
-  echo -n $cyan$USER$normal@$yellow$__fish_prompt_hostname$normal: $bred(prompt_pwd)$normal
+  echo -n $cyan$USER$normal@$yellow$__fish_prompt_hostname$normal: $bgreen(prompt_pwd)$normal
   __fish_git_prompt
 
   echo
